96e0faf7d33125ecf80c3c62ab8883009847f1e5
Alexander Byakov <byakov-a55@mail.ru>
2026-05-02 14:39:35 +0300

    add entities and typeConverter in parameter of database

--- app/src/main/java/com/rimuru/android/rhythmlens/data/local/RhythmLensDatabase.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/data/local/RhythmLensDatabase.kt
@@ -2,12 +2,23 @@ package com.rimuru.android.rhythmlens.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.rimuru.android.rhythmlens.data.local.converter.Converters
+import com.rimuru.android.rhythmlens.data.local.dao.EcgDao
+import com.rimuru.android.rhythmlens.data.local.entity.EcgRecordEntity
+import com.rimuru.android.rhythmlens.data.local.entity.PatientEntity
 
 @Database(
-    entities = [],           // позже добавим entities
+    entities = [
+        EcgRecordEntity::class,
+        PatientEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class RhythmLensDatabase : RoomDatabase() {
-    // DAOs будут здесь позже
+
+    abstract fun ecgDao(): EcgDao
+    // abstract fun patientDao(): PatientDao  ← позже
 }
