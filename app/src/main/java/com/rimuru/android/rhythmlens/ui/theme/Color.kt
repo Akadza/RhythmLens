614b4c79bbefefe303cc5c26b59ed415e239f46b
Alexander Byakov <byakov-a55@mail.ru>
2026-05-02 16:39:44 +0300

    add new colors for UI

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/theme/Color.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.rimuru.android.rhythmlens.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryBlue = Color(0xFF1E88E5)
+val PrimaryDarkBlue = Color(0xFF1565C0)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SecondaryTeal = Color(0xFF00BFA5)
+val AccentGreen = Color(0xFF34C759)
+
+val BackgroundLight = Color(0xFFF8F9FA)
+val SurfaceLight = Color(0xFFFFFFFF)
+val BackgroundDark = Color(0xFF1A1C1E)
+val SurfaceDark = Color(0xFF222427)
+
+val ErrorRed = Color(0xFFEA4335)
+val OnPrimary = Color.White
