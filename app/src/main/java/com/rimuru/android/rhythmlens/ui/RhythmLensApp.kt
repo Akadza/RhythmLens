37fce29ffdfbe73de469ab38499c5d67a04da35a
Alexander Byakov <byakov-a55@mail.ru>
2026-05-28 20:52:11 +0300

    add Compose root application entry

---
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/RhythmLensApp.kt
@@ -0,0 +1,9 @@
+package com.rimuru.android.rhythmlens.ui
+
+import androidx.compose.runtime.Composable
+import com.rimuru.android.rhythmlens.ui.navigation.AppNavHost
+
+@Composable
+fun RhythmLensApp() {
+    AppNavHost()
+}
