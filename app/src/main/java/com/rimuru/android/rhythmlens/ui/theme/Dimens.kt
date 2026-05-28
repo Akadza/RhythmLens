e68c9dea054ef68e222832cb11611018f8a7999a
Alexander Byakov <byakov-a55@mail.ru>
2026-05-28 20:52:21 +0300

    feat(theme): add shared spacing and size tokens

---
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/theme/Dimens.kt
@@ -0,0 +1,35 @@
+package com.rimuru.android.rhythmlens.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object RhythmSpacing {
+    val None = 0.dp
+    val ExtraSmall = 4.dp
+    val Small = 8.dp
+    val Medium = 12.dp
+    val Large = 16.dp
+    val ExtraLarge = 20.dp
+    val XLarge = 24.dp
+    val XXLarge = 32.dp
+
+    val ScreenHorizontal = 20.dp
+    val ScreenVertical = 20.dp
+}
+
+object RhythmSize {
+    val Fab = 72.dp
+    val FabIcon = 36.dp
+    val SmallIcon = 24.dp
+    val MediumIcon = 28.dp
+
+    val AnalysisIndicator = 168.dp
+    val AnalysisStroke = 16.dp
+    val ProbabilityBarWidth = 96.dp
+    val LeadPreviewHeight = 72.dp
+}
+
+object RhythmCorner {
+    val Card = 24.dp
+    val Button = 20.dp
+    val BottomSheet = 28.dp
+}
