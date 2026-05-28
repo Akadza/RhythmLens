3892612b5f3a1d352e336f9a01ae097fa9789ede
Alexander Byakov <byakov-a55@mail.ru>
2026-05-28 20:57:00 +0300

    refactor(nav): define bottom tab graph

---
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/navigation/BottomNavGraph.kt
@@ -0,0 +1,75 @@
+package com.rimuru.android.rhythmlens.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.rimuru.android.rhythmlens.R
+import com.rimuru.android.rhythmlens.ui.screens.history.HistoryScreen
+import com.rimuru.android.rhythmlens.ui.screens.home.HomeScreen
+import com.rimuru.android.rhythmlens.ui.theme.RhythmSpacing
+
+fun NavGraphBuilder.bottomNavGraph(
+    rootNavController: NavHostController
+) {
+    composable<BottomNavDestination.Home> {
+        HomeScreen(
+            onScanClick = {
+                rootNavController.navigate(ScanDestination)
+            },
+            onGalleryClick = {
+                // TODO: открыть системный Photo Picker
+            },
+            onImportClick = {
+                // TODO: открыть системный File Picker
+            }
+        )
+    }
+
+    composable<BottomNavDestination.History> {
+        HistoryScreen(
+            onEcgClick = { ecgId ->
+                rootNavController.navigate(
+                    EcgDetailDestination(ecgId)
+                )
+            }
+        )
+    }
+
+    composable<BottomNavDestination.Patients> {
+        PlaceholderScreen(
+            title = stringResource(R.string.patients)
+        )
+    }
+
+    composable<BottomNavDestination.Profile> {
+        PlaceholderScreen(
+            title = stringResource(R.string.profile)
+        )
+    }
+}
+
+@Composable
+private fun PlaceholderScreen(
+    title: String
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(RhythmSpacing.XLarge),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(R.string.placeholder_screen_template, title),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
