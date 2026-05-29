e672d31cdc78e4c0e5010cda3d573ea4b089b519
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-29 09:03:02 +0000

    chore(deps): wire lifecycle state dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation(libs.hilt.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
