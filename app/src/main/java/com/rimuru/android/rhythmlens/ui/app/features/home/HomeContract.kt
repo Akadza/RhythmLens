83ccb1a95a91a758380b3675b548695d66b734d0
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-29 09:03:19 +0000

    feat(home): add MVI contract

---
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeContract.kt
@@ -0,0 +1,34 @@
+package com.rimuru.android.rhythmlens.ui.screens.home
+
+data class HomeUiState(
+    val userName: String = "",
+    val totalRecords: Int = 0,
+    val linkedDoctorCount: Int = 0,
+    val lastRecord: LastEcgUi? = null,
+    val isAddEcgSheetVisible: Boolean = false
+)
+
+data class LastEcgUi(
+    val id: String,
+    val date: String,
+    val mainResult: String,
+    val probability: Int,
+    val digitizedLeads: Int,
+    val reconstructedLeads: Int
+)
+
+sealed interface HomeEvent {
+    data object AddEcgClicked : HomeEvent
+    data object AddEcgSheetDismissed : HomeEvent
+    data object ScanClicked : HomeEvent
+    data object GalleryClicked : HomeEvent
+    data object ImportClicked : HomeEvent
+    data class LastRecordClicked(val ecgId: String) : HomeEvent
+}
+
+sealed interface HomeEffect {
+    data object NavigateToScan : HomeEffect
+    data object OpenGalleryPicker : HomeEffect
+    data object OpenFilePicker : HomeEffect
+    data class NavigateToEcgDetail(val ecgId: String) : HomeEffect
+}
