5813032bd1de05071439378759a6b9ccabc8b212
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-29 09:47:42 +0000

    fix(history): align list item package with path

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/history/EcgListItem.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/history/EcgListItem.kt
@@ -1,4 +1,4 @@
-package com.rimuru.android.rhythmlens.ui.screens.history
+package com.rimuru.android.rhythmlens.ui.app.features.history
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
