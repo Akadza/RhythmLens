50f5d61ff37e5c75888feb6a8cb59fc3fa9f6f96
Alexander Byakov <byakov-a55@mail.ru>
2026-05-29 12:44:47 +0300

    fix(home): align home screen package with path

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeScreen.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeScreen.kt
@@ -1,4 +1,4 @@
-package com.rimuru.android.rhythmlens.ui.screens.home
+package com.rimuru.android.rhythmlens.ui.app.features.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
