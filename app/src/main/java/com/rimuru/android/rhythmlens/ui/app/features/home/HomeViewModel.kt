55f50ff0703ac81b2a3685adbb367675a5e64238
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-29 16:09:41 +0000

    refactor(home): expose UI state as read-only StateFlow

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeViewModel.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeViewModel.kt
@@ -8,6 +8,7 @@ import com.rimuru.android.rhythmlens.domain.usecase.GetEcgListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
@@ -22,7 +23,7 @@ class HomeViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(initialState())
-    val uiState = _uiState
+    val uiState = _uiState.asStateFlow()
 
     private val _effect = Channel<HomeEffect>(capacity = Channel.BUFFERED)
     val effect = _effect.receiveAsFlow()
