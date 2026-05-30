5c7533fa2be3eb44979bb9f69fab2fdcad49278e
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-30 11:00:51 +0000

    feat(data): register ECG signal table in Room database

--- app/src/main/java/com/rimuru/android/rhythmlens/data/local/RhythmLensDatabase.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/data/local/RhythmLensDatabase.kt
@@ -5,20 +5,24 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.rimuru.android.rhythmlens.data.local.converter.Converters
 import com.rimuru.android.rhythmlens.data.local.dao.EcgDao
+import com.rimuru.android.rhythmlens.data.local.dao.EcgSignalDao
 import com.rimuru.android.rhythmlens.data.local.entity.EcgRecordEntity
+import com.rimuru.android.rhythmlens.data.local.entity.EcgSignalLeadEntity
 import com.rimuru.android.rhythmlens.data.local.entity.PatientEntity
 
 @Database(
     entities = [
         EcgRecordEntity::class,
+        EcgSignalLeadEntity::class,
         PatientEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class RhythmLensDatabase : RoomDatabase() {
 
     abstract fun ecgDao(): EcgDao
+    abstract fun ecgSignalDao(): EcgSignalDao
     // abstract fun patientDao(): PatientDao  ← позже
 }
