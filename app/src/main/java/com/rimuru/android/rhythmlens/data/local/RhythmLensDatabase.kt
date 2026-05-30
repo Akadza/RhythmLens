4c02c91dc0ef9d96aee2a5b72f2928929fabfe2f
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-30 14:10:15 +0000

    feat(data): register doctor conclusion table

--- app/src/main/java/com/rimuru/android/rhythmlens/data/local/RhythmLensDatabase.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/data/local/RhythmLensDatabase.kt
@@ -4,9 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.rimuru.android.rhythmlens.data.local.converter.Converters
+import com.rimuru.android.rhythmlens.data.local.dao.DoctorConclusionDao
 import com.rimuru.android.rhythmlens.data.local.dao.EcgDao
 import com.rimuru.android.rhythmlens.data.local.dao.EcgSignalDao
 import com.rimuru.android.rhythmlens.data.local.dao.PatientDao
+import com.rimuru.android.rhythmlens.data.local.entity.DoctorConclusionEntity
 import com.rimuru.android.rhythmlens.data.local.entity.EcgRecordEntity
 import com.rimuru.android.rhythmlens.data.local.entity.EcgSignalLeadEntity
 import com.rimuru.android.rhythmlens.data.local.entity.PatientEntity
@@ -15,9 +17,10 @@ import com.rimuru.android.rhythmlens.data.local.entity.PatientEntity
     entities = [
         EcgRecordEntity::class,
         EcgSignalLeadEntity::class,
-        PatientEntity::class
+        PatientEntity::class,
+        DoctorConclusionEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
@@ -26,4 +29,5 @@ abstract class RhythmLensDatabase : RoomDatabase() {
     abstract fun ecgDao(): EcgDao
     abstract fun ecgSignalDao(): EcgSignalDao
     abstract fun patientDao(): PatientDao
+    abstract fun doctorConclusionDao(): DoctorConclusionDao
 }
