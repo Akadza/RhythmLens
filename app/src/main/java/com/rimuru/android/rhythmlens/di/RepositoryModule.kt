880318aece12d0b08761ac63d2be75dbad4faddd
Александр <61403845+Akadza@users.noreply.github.com>
2026-05-30 12:52:58 +0000

    feat(di): bind DataStore session repository

--- app/src/main/java/com/rimuru/android/rhythmlens/di/RepositoryModule.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.rimuru.android.rhythmlens.di
 
+import com.rimuru.android.rhythmlens.data.repository.DataStoreSessionRepositoryImpl
 import com.rimuru.android.rhythmlens.data.repository.EcgRepositoryImpl
 import com.rimuru.android.rhythmlens.domain.repository.EcgRepository
+import com.rimuru.android.rhythmlens.domain.repository.SessionRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ abstract class RepositoryModule {
     abstract fun bindEcgRepository(
         impl: EcgRepositoryImpl
     ): EcgRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindSessionRepository(
+        impl: DataStoreSessionRepositoryImpl
+    ): SessionRepository
 }
