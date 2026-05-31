f16493ef5ea2845681b79f2f16d674feebd1aeb2
Alexander Byakov <byakov-a55@mail.ru>
2026-05-31 18:44:16 +0300

    feat(auth): implement use case logout

--- app/src/main/java/com/rimuru/android/rhythmlens/domain/usecase/LogoutUseCase.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/domain/usecase/LogoutUseCase.kt
@@ -1,12 +1,15 @@
 package com.rimuru.android.rhythmlens.domain.usecase
 
+import com.rimuru.android.rhythmlens.domain.repository.AuthRepository
 import com.rimuru.android.rhythmlens.domain.repository.SessionRepository
 import javax.inject.Inject
 
 class LogoutUseCase @Inject constructor(
+    private val authRepository: AuthRepository,
     private val sessionRepository: SessionRepository
 ) {
     suspend operator fun invoke() {
+        authRepository.signOut()
         sessionRepository.clearSession()
     }
 }
