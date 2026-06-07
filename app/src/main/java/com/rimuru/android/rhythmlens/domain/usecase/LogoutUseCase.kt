9402f5a1e435afd335f8f511f8f323041939d4e2
Александр <61403845+Akadza@users.noreply.github.com>
2026-06-07 10:16:58 +0000

    Clear local data on logout

--- app/src/main/java/com/rimuru/android/rhythmlens/domain/usecase/LogoutUseCase.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/domain/usecase/LogoutUseCase.kt
@@ -1,15 +1,18 @@
 package com.rimuru.android.rhythmlens.domain.usecase
 
 import com.rimuru.android.rhythmlens.domain.repository.AuthRepository
+import com.rimuru.android.rhythmlens.domain.repository.LocalCacheRepository
 import com.rimuru.android.rhythmlens.domain.repository.SessionRepository
 import javax.inject.Inject
 
 class LogoutUseCase @Inject constructor(
     private val authRepository: AuthRepository,
-    private val sessionRepository: SessionRepository
+    private val sessionRepository: SessionRepository,
+    private val localCacheRepository: LocalCacheRepository
 ) {
     suspend operator fun invoke() {
         authRepository.signOut()
+        localCacheRepository.clearAllLocalData()
         sessionRepository.clearSession()
     }
 }
