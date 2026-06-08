7980620f2789e268eb0d6e0b7d7e042d687513a8
Александр <61403845+Akadza@users.noreply.github.com>
2026-06-08 21:52:35 +0000

    Refine RhythmLens color surfaces

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/theme/Color.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/theme/Color.kt
@@ -8,10 +8,13 @@ val PrimaryDarkBlue = Color(0xFF1565C0)
 val SecondaryTeal = Color(0xFF00BFA5)
 val AccentGreen = Color(0xFF34C759)
 
-val BackgroundLight = Color(0xFFF8F9FA)
+val BackgroundLight = Color(0xFFF6F8FB)
 val SurfaceLight = Color(0xFFFFFFFF)
-val BackgroundDark = Color(0xFF1A1C1E)
-val SurfaceDark = Color(0xFF222427)
+val SurfaceVariantLight = Color(0xFFEFF3F8)
+val BackgroundDark = Color(0xFF17191D)
+val SurfaceDark = Color(0xFF22252B)
+val SurfaceVariantDark = Color(0xFF30333B)
 
+val WarningAmber = Color(0xFFFFB300)
 val ErrorRed = Color(0xFFEA4335)
 val OnPrimary = Color.White
