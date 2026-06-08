b589ed665b4ce2924cb8bbed83021a7bfd6c2abc
Александр <61403845+Akadza@users.noreply.github.com>
2026-06-08 22:49:57 +0000

    Increase history app bar height

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/history/HistoryScreen.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/history/HistoryScreen.kt
@@ -36,7 +36,7 @@ fun HistoryScreen(
         containerColor = MaterialTheme.colorScheme.background,
         topBar = {
             TopAppBar(
-                modifier = Modifier.height(56.dp),
+                modifier = Modifier.height(64.dp),
                 title = {
                     Text(text = stringResource(R.string.history))
                 }
