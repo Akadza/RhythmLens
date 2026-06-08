932d3d13e91a09ccf49d82904e98db41f300decd
Александр <61403845+Akadza@users.noreply.github.com>
2026-06-08 23:02:07 +0000

    Increase patients app bar visible height

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/patients/PatientsScreen.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/patients/PatientsScreen.kt
@@ -41,7 +41,7 @@ fun PatientsScreen(
         containerColor = MaterialTheme.colorScheme.background,
         topBar = {
             TopAppBar(
-                modifier = Modifier.height(64.dp),
+                modifier = Modifier.height(88.dp),
                 title = {
                     Text(text = stringResource(R.string.patients))
                 }
