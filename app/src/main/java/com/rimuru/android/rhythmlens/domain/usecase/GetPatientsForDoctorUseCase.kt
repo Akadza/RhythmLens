4211a1972736390b24031afb54d96099a3d1e6a0
Александр <61403845+Akadza@users.noreply.github.com>
2026-06-08 09:28:36 +0000

    Refresh doctor patients from backend

--- app/src/main/java/com/rimuru/android/rhythmlens/domain/usecase/GetPatientsForDoctorUseCase.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/domain/usecase/GetPatientsForDoctorUseCase.kt
@@ -3,6 +3,7 @@ package com.rimuru.android.rhythmlens.domain.usecase
 import com.rimuru.android.rhythmlens.domain.model.Patient
 import com.rimuru.android.rhythmlens.domain.repository.PatientRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class GetPatientsForDoctorUseCase @Inject constructor(
@@ -10,5 +11,8 @@ class GetPatientsForDoctorUseCase @Inject constructor(
 ) {
     operator fun invoke(doctorId: String): Flow<List<Patient>> {
         return patientRepository.observePatientsForDoctor(doctorId)
+            .onStart {
+                patientRepository.refreshPatientsForDoctor(doctorId)
+            }
     }
 }
