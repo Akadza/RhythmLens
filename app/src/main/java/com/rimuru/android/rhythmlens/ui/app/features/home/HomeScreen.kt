4a3345e8f9ce9e744318168d970cebd33fef5e4f
Alexander Byakov <byakov-a55@mail.ru>
2026-06-09 21:34:15 +0300

    feat(auth): add import dp

--- app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeScreen.kt
+++ app/src/main/java/com/rimuru/android/rhythmlens/ui/app/features/home/HomeScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.rimuru.android.rhythmlens.R
 import com.rimuru.android.rhythmlens.ui.theme.RhythmSize
 import com.rimuru.android.rhythmlens.ui.theme.RhythmSpacing
